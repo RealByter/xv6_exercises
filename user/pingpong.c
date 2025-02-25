cf83002569bf2dae81f94862d04554d9dcfb34d8
Byter <justavishay@gmail.com>
2025-02-25 23:54:26 +0200

    added pongpong

---
+++ user/pingpong.c
@@ -0,0 +1,34 @@
+#include "kernel/stat.h"
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+  int p[2];
+
+  pipe(p);
+  if (fork() == 0) {
+    int id = getpid();
+    char buf[4] = {0};
+
+    read(p[0], buf, 4);
+    close(p[0]);
+    printf("%d: received %s\n", id, buf);
+
+    write(p[1], "pong", 4);
+    close(p[1]);
+
+    exit(0);
+  } else {
+    int id = getpid();
+    char buf[4] = {0};
+
+    write(p[1], "ping", 4);
+    close(p[1]);
+
+    read(p[0], buf, 4);
+    close(p[0]);
+    printf("%d: received %s\n", id, buf);
+
+    exit(0);
+  }
+}
